9068d23bafdefe9cc77827b96f400777280a0378
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2024-03-23 18:38:48 -0400

    Added Main Menu UI scenes

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenuControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Level 1"); // Load Level 1 scene
     }
 
-    // Update is called once per frame
-    void Update()
+    // Method called when the Levels button is clicked
+    public void OpenLevels()
     {
-        
+        SceneManager.LoadScene("LevelSelector"); // Load Levels scene
     }
+
+    // Method called when the Settings button is clicked
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings"); // Load Settings scene
+    }
+
+    // Method called when the Exit button is clicked
+    public void ExitGame()
+    {
+        // Exit the game (only works in standalone builds)
+        Debug.Log("Game is exiting...");
+        Application.Quit();
+    }
+
 }
