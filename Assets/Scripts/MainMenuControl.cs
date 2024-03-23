e732ee4570c7d24716adeb897edbd13f8647e287
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2024-03-23 18:45:51 -0400

    Added level selection menu

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -31,4 +31,35 @@ public class MainMenuControl : MonoBehaviour
         Application.Quit();
     }
 
+    public void OpenLevel1()
+    {
+        SceneManager.LoadScene("Level 1"); // Load Level 1 scene
+    }
+
+    public void OpenLevel2()
+    {
+        SceneManager.LoadScene("Level 2"); // Load Level 2 scene
+    }
+
+    public void OpenLevel3()
+    {
+        SceneManager.LoadScene("Level 3"); // Load Level 3 scene
+    }
+
+    public void OpenLevel4()
+    {
+        SceneManager.LoadScene("Level 4"); // Load Level 4 scene
+    }
+
+    public void OpenLevel5()
+    {
+        SceneManager.LoadScene("Level 5"); // Load Level 5 scene
+    }
+
+    public void OpenLevel6()
+    {
+        SceneManager.LoadScene("Level 6"); // Load Level 6 scene
+    }
+
+
 }
