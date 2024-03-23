5d67eab3cd359932aa2cd93a9e6b6996608ec954
Dylan Walker <dylan.walker1320@gmail.com>
2024-03-23 15:12:59 -0400

    Working Reflection Mirror

--- Assets/Scripts/MirrorShadow.cs
+++ Assets/Scripts/MirrorShadow.cs
@@ -10,9 +10,13 @@ public class MirrorShadow : MonoBehaviour
     
     public List<Collider2D> touching;
 
+    private ReflectMirror reflectMirror;
+
+
     void Start()
     {
         touching = new List<Collider2D>();
+        reflectMirror = GetComponentInParent<ReflectMirror>();
 
         sr = GetComponent<SpriteRenderer>();
         sr.color = new Color(0, 0, 0, 0.25f);
@@ -24,16 +28,35 @@ public class MirrorShadow : MonoBehaviour
         transform.localPosition = new Vector3(0, -transform.localScale.y / 2 - 1, 0);
     }
 
-    void OnTriggerEnter2D(Collider2D collider)
+    void OnTriggerStay2D(Collider2D collider)
     {
-        print("Trigger Enter");
-        touching.Add(collider);
+        // Check if the collider is colliding with the reflectMirror
+        Collider2D[] colliders = Physics2D.OverlapAreaAll(collider.bounds.min, collider.bounds.max);
+        bool isCollidingWithReflectMirror = false;
+        foreach (Collider2D col in colliders)
+        {
+            if (col.gameObject == reflectMirror.gameObject)
+            {
+                isCollidingWithReflectMirror = true;
+                reflectMirror.RemoveReflectedObject(collider.gameObject);
+                break;
+            }
+        }
+
+        if (!isCollidingWithReflectMirror)
+        {
+            touching.Add(collider);
+            reflectMirror.Reflect(collider.gameObject);
+        }
     }
 
     void OnTriggerExit2D(Collider2D collider)
     {
-        print("Trigger Exit");
-        touching.Remove(collider);
+        if (collider.gameObject.tag == "Mirrorable" && touching.Contains(collider))
+        {
+            touching.Remove(collider);
+            reflectMirror.RemoveReflectedObject(collider.gameObject);
+        }
     }
 
 }
