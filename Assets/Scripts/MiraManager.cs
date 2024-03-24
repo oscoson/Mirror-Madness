e08c4df09e4f95d0e2606ba08ee342e801ad97f6
Brian Cho <b.cho@queensu.ca>
2024-03-24 14:18:27 -0400

    Add Reflect preview

--- Assets/Scripts/MiraManager.cs
+++ Assets/Scripts/MiraManager.cs
@@ -18,6 +18,10 @@ public class MiraManager : MonoBehaviour
 
     private float minReflectLen = 0.05f;
 
+    private GameObject reflectorPreview;
+    private GameObject rotatorPreview;
+
+
     private Vector2 startPos;
     public GameObject reflectorPrefab;
     public GameObject rotatorPrefab;
@@ -32,6 +36,18 @@ public class MiraManager : MonoBehaviour
     private void Awake()
     {
         rotators = new();
+
+        reflectorPreview = transform.GetComponentInChildren<ReflectorPreview>().gameObject;
+        rotatorPreview = transform.GetComponentInChildren<RotatorPreview>().gameObject;
+
+        SpriteRenderer refsr = reflectorPreview.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        refsr.color = new Color(0, 0, 0, 0.25f);
+
+        SpriteRenderer rotsr = rotatorPreview.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        rotsr.color = new Color(0, 0, 0, 0.25f);
+
+        reflectorPreview.SetActive(false);
+        rotatorPreview.SetActive(false);
     }
 
     void Update()
@@ -48,7 +64,8 @@ public class MiraManager : MonoBehaviour
             currentTool = "Rotator";
         }
 
-    // ----------MOUSE INPUT----------
+
+        // ----------MOUSE INPUT----------
         // Listen for a left mouse down
         if (Input.GetMouseButtonDown(0))
         {
@@ -69,7 +86,7 @@ public class MiraManager : MonoBehaviour
                 }
             }
 
-            placing = true;
+            placing = !rotating;
 
             // Get the mouse position
 
@@ -100,6 +117,7 @@ public class MiraManager : MonoBehaviour
             }
         }
 
+
         if (rotating)
         {
             foreach (Rotator rotator in rotators)
@@ -122,6 +140,7 @@ public class MiraManager : MonoBehaviour
             }
             else if (placing)
             {
+
                 // Get the mouse position
                 Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -144,6 +163,8 @@ public class MiraManager : MonoBehaviour
                         }
                     }
                 }
+
+                placing = false;
             }
         }
 
@@ -154,10 +175,11 @@ public class MiraManager : MonoBehaviour
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             startPos = mousePos;
+            placing = true;
         }
 
         // Listen for a right mouse up
-        if (Input.GetMouseButtonUp(1))
+        if (Input.GetMouseButtonUp(1) && placing)
         {
             // Get the mouse position
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -172,6 +194,62 @@ public class MiraManager : MonoBehaviour
                         PlaceReflector(startPos, mousePos, -1);
                     }
                 }
+            }
+            placing = false;
+        }
+
+
+        if (!placing)
+        {
+            reflectorPreview.SetActive(false);
+            rotatorPreview.SetActive(false);
+        }
+        else
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (currentTool == "Reflector")
+            {
+                reflectorPreview.SetActive(true);
+                rotatorPreview.SetActive(false);
+
+                reflectorPreview.transform.position = (mousePos - startPos) * 0.5f + startPos;
+                // Set the scale of the reflector
+                reflectorPreview.transform.localScale = new Vector3(Vector2.Distance(startPos, mousePos), REFLECTOR_WIDTH, 1);
+
+                int inverse = Input.GetMouseButton(0) ? 1 : -1;
+                // Set the rotation of the reflector
+                reflectorPreview.transform.rotation = Quaternion.FromToRotation(Vector2.right, inverse * (mousePos - startPos));
+
+
+
+                Transform shadow = reflectorPreview.transform.GetChild(0);
+
+                // Set the size of the shadow to be a square with length the same as the x scale of the parent
+                shadow.transform.localScale = new Vector3(1, reflectorPreview.transform.localScale.x / reflectorPreview.transform.localScale.y, 1);
+
+                // Sit on the bottom of the parent
+                shadow.transform.localPosition = new Vector3(0, -shadow.transform.localScale.y / 2 - 1, 0);
+
+
+            }
+            else if (currentTool == "Rotator")
+            {
+                rotatorPreview.SetActive(true);
+                reflectorPreview.SetActive(false);
+
+                rotatorPreview.transform.position = startPos;
+
+
+                // Set the scale of the rotator
+                Vector2 dist = (mousePos - startPos) * 2;
+                float mag = dist.magnitude;
+
+                // Get the transform of the child
+                Transform shadow = rotatorPreview.transform.GetChild(0);
+
+                // Set the scale of the child
+                shadow.localScale = new Vector3(mag, mag, mag);
             }
         }
     }
